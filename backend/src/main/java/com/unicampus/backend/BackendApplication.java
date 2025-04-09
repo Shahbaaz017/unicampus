3c410361ba3275ec3a16bdc13ba76c36b37312cb
iamsjeevan <71625285+iamsjeevan@users.noreply.github.com>
2025-04-09 19:19:29 +0000

    error Correction

--- backend/src/main/java/com/unicampus/backend/BackendApplication.java
+++ backend/src/main/java/com/unicampus/backend/BackendApplication.java
@@ -1,13 +1,16 @@
-package com.unicampus.backend;
+package com.unicampus.backend; // Your package
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Add this import:
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// Add the exclude annotation here:
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
